3545348f30ce484fb2ae9049a12a79d33fded15b
tarunja1ks <tarunjaiks@gmail.com>
2025-10-13 06:04:28 -0700

    score method

--- ParticleFilter.py
+++ ParticleFilter.py
@@ -155,55 +155,26 @@ class ParticleFilter:
 
         world_angles=sensor_angles + angles.reshape(1, -1)
         
+        x_hits=sensor_x+(ranges).reshape(1,-1)*torch.cos(world_angles)
+        y_hits=sensor_y+(ranges).reshape(1,-1)*torch.sin(world_angles)
         
-        self.scales=torch.linspace(0, 1, max_cell_range, dtype=torch.float32, device=self.device).reshape(1, 1, -1)
-        
-        self.dx=torch.cos(world_angles)[:, :, None] * max_cell_range * self.scales
-        self.dy=torch.sin(world_angles)[:, :, None] * max_cell_range * self.scales
-        
-        cell_sensor_x, cell_sensor_y=OGM.vector_meter_to_cell(sensor_poses.cpu().numpy().T)
-        cell_sensor_x=torch.tensor(cell_sensor_x,dtype=torch.float32, device=self.device)
-        cell_sensor_y=torch.tensor(cell_sensor_y,dtype=torch.float32, device=self.device)
-        x_cells=torch.floor(self.dx + cell_sensor_x[:, None, None]).long()
-        y_cells=torch.floor(self.dy + cell_sensor_y[:, None, None]).long()
-
-        H, W=OGM.MAP['map'].shape
-        x_cells=torch.clamp(x_cells, 0, H-1)
-        y_cells=torch.clamp(y_cells, 0, W-1)
-        
-        
-        occupied=torch.tensor(OGM.MAP['map'], dtype=torch.float32, device=self.device)[x_cells, y_cells] > 0
-        
-        first_occupied=torch.argmax(occupied.long(), dim=2)
-        no_obstacle=~torch.any(occupied, dim=2)
-        first_occupied[no_obstacle]=max_cell_range - 1
+        x_hits,y_hits=OGM.vector_meter_to_cell(np.array([x_hits.cpu().numpy(),y_hits.cpu().numpy()]))
+        hits=(OGM.MAP['map'][x_hits.flatten(),y_hits.flatten()]>0).reshape(x_hits.shape)
+        scores=np.sum(hits,axis=1)
         
 
-        particle_idx, ray_idx=torch.meshgrid(torch.arange(first_occupied.shape[0]), torch.arange(first_occupied.shape[1]), indexing='ij')
-        x_hits=x_cells[particle_idx, ray_idx, first_occupied].float()
-        y_hits=y_cells[particle_idx, ray_idx, first_occupied].float()
-        
-        ztk_star=(((y_hits-cell_sensor_y[:,None])**2+(x_hits-cell_sensor_x[:,None])**2)**0.5)/20
-        
-        ztk=ranges.reshape(1, -1)
-        
-        
-        log_likelihood=-0.5 * ((ztk - ztk_star) / self.lidar_stdev)**2
-        log_weights=torch.sum(log_likelihood, dim=1)
-
-        max_log_weight=torch.max(log_weights)
-        self.particle_weights=torch.exp(log_weights - max_log_weight)
-        self.particle_weights /= torch.sum(self.particle_weights)
+      
+        self.particle_weights*=np.exp(0.01*scores) # 0.01 is an arbritary scale
+        self.particle_weights /= np.sum(self.particle_weights)
         
 
-        weighted_x=np.sum(self.particle_poses[:, 0] * self.particle_weights.cpu().numpy().flatten())
-        weighted_y=np.sum(self.particle_poses[:, 1] * self.particle_weights.cpu().numpy().flatten())
+        weighted_x=np.sum(self.particle_poses[:, 0] * self.particle_weights)
+        weighted_y=np.sum(self.particle_poses[:, 1] * self.particle_weights)
         
-        weighted_sin=np.sum(np.sin(self.particle_poses[:, 2]) * self.particle_weights.cpu().numpy().flatten())
-        weighted_cos=np.sum(np.cos(self.particle_poses[:, 2]) * self.particle_weights.cpu().numpy().flatten())
+        weighted_sin=np.sum(np.sin(self.particle_poses[:, 2]) * self.particle_weights)
+        weighted_cos=np.sum(np.cos(self.particle_poses[:, 2]) * self.particle_weights)
         weighted_angle=np.arctan2(weighted_sin, weighted_cos)
         
-        self.particle_weights=self.particle_weights.cpu().numpy()
         weighted_pose=np.array([weighted_x, weighted_y, weighted_angle])
         
 
